250619487c899464a0f0a86fe1b7e9047e6b819d
shinichi-c <gokus12456@gmail.com>
2025-06-19 13:56:10 +0000

    sdm845-common: Initialize Evolution X

--- common.mk
+++ common.mk
@@ -255,10 +255,6 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.oneplus_sdm845
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
