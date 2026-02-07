78aa195d2ceb0127b8c05c6722e0dddb53a8da19
shinichi-c <gokus12456@gmail.com>
2026-02-07 17:47:32 +0000

    sdm845-common: add remove_packages

--- common.mk
+++ common.mk
@@ -32,6 +32,11 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+      RemovePackages
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
